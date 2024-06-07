ac3f4ec4a1921a6995c814768372f9e23b01e1bf
Fernando C <thelonder461@gmail.com>
2024-06-07 10:53:00 -0300

    Login realizado e bugs corrigidos na landing page

---
+++ src/js/login.js
@@ -0,0 +1,17 @@
+// variaveis
+const signupButton = document.getElementById('signup-button'),
+    loginButton = document.getElementById('login-button'),
+    userForms = document.getElementById('user_options-forms')
+
+// eventos escutadores
+signupButton.addEventListener('click', () => {
+  userForms.classList.remove('bounceRight')
+  userForms.classList.add('bounceLeft')
+}, false)
+
+
+loginButton.addEventListener('click', () => {
+  userForms.classList.remove('bounceLeft')
+  userForms.classList.add('bounceRight')
+}, false)
+
--- src/js/script.js
+++ src/js/script.js
@@ -5,58 +5,67 @@ const primaryNav = document.querySelector(".primary-navigation");
 const secondaryNav = document.querySelector(".secondary-navigation");
 const about = document.querySelector(".about");
 
-navToggle.addEventListener('click', () => {
-    primaryNav.hasAttribute('data-visible')
-        ? navToggle.setAttribute("aria-expanded", false)
-        : navToggle.setAttribute("aria-expanded", true);
-    primaryNav.toggleAttribute("data-visible");
-    primaryHeader.toggleAttribute("data-overlay");
-    secondaryNav.toggleAttribute("data-visible");
+navToggle.addEventListener("click", () => {
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
+  primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
+  secondaryNav.toggleAttribute("data-visible");
 });
 
 //CONTROLE DA VISIBILIDADE DO SEGUNDO NAV NO DESKTOP
-const Arrow = document.getElementById('arrow');
-about.addEventListener('click', () => {
-    secondaryNav.toggleAttribute("data-visible");
-    Arrow.classList.toggle("active");
+const Arrow = document.getElementById("arrow");
+about.addEventListener("click", () => {
+  secondaryNav.toggleAttribute("data-visible");
+  Arrow.classList.toggle("active");
 });
 
-//SLIDER
+// SLIDER
 
 var swiper = new Swiper(".swiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 100,
-      modifier: 3,
-      slideShadows: true
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 3,
+    slideShadows: true,
+  },
+  keyboard: {
+    enabled: true,
+  },
+  mousewheel: {
+    thresholdDelta: 70,
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
     },
-    keyboard: {
-      enabled: true
+    768: {
+      slidesPerView: 1,
     },
-    mousewheel: {
-      thresholdDelta: 70
+    1024: {
+      slidesPerView: 2,
     },
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true
+    1560: {
+      slidesPerView: 3,
     },
-    breakpoints: {
-      640: {
-        slidesPerView: 2
-      },
-      768: {
-        slidesPerView: 1
-      },
-      1024: {
-        slidesPerView: 2
-      },
-      1560: {
-        slidesPerView: 3
-      }
-    }
-  });
+  },
+});
+
+// AUDIO DE FUNDO
+const slide = document.querySelector(".main-slide");
+const audio = document.getElementById("oceanWaves");
+
+slide.addEventListener("click", function () {
+  audio.currentTime = 0; // Reinicia o áudio para o início
+  audio.play();
+});
