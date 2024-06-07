9b49af63a58afc6f374cf733145c43de5ee49e87
Fernando C <thelonder461@gmail.com>
2024-06-07 17:23:33 -0300

    testando funcionalidade dos redirecionamentos

--- src/js/contato.js
+++ src/js/contato.js
@@ -1,4 +1,4 @@
 document.getElementById('forms-id').addEventListener('submit', function(event) {
     event.preventDefault(); // Impede o comportamento padrão do formulário
-    window.location.href = '../../index.html'; // Redireciona para a página desejada
+    window.location.href = './../../index.html'; // Redireciona para a página desejada
 });
